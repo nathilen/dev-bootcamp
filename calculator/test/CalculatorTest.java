cd52b5f5f7baf989b119941ea69ad2ee417b80e8
Charlene <charlet@thoughtworks.com>
2014-08-05 15:03:25 +0200

    Charlene: Initial commit

---
+++ calculator/test/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void testName() throws Exception {
+        assertThat(1, equalTo(1));
+    }
+}
