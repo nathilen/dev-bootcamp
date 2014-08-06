2c922215be4d291067ce73650377a369470469f7
tsatsimahase <tsatsi.mahase@gmail.com>
2014-08-06 14:48:13 +0200

    removed unused imports

--- calculator/test/CalculatorTest.java
+++ calculator/test/CalculatorTest.java
@@ -1,9 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
 
