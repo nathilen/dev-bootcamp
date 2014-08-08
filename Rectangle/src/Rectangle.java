913a70644cbbf2ba62d7ef6566522b50ea5cdab9
Liza Madubela <Liza.Madubela@standardbank.co.za>
2014-08-08 12:19:13 +0200

    Adding perimter and starting with square

--- Rectangle/src/Rectangle.java
+++ Rectangle/src/Rectangle.java
@@ -1,11 +1,13 @@
+//Job: Understands a 4-sided polygon with adjacent sides connecting at 90 degrees
+
 public class Rectangle {
     private int length;
     private int width;
 
     public Rectangle(int length, int width) {
 
-        if(length <= 0 || width <= 0){
-           throw  new RuntimeException("Length or width cannot be negative");
+        if (length <= 0 || width <= 0) {
+            throw new RuntimeException("Length and width must be positive");
         }
         this.length = length;
         this.width = width;
@@ -14,5 +16,17 @@ public class Rectangle {
     public int area() {
 
         return length * width;
+    }
+
+    public int perimeter() {
+        return 2 * (length + width);
+    }
+
+    public static Rectangle asSquare(int side) {
+        return new Rectangle(side, side);
+    }
+
+    public String toString() {
+        return "Rectangle with width=" + width + ", length=" + length;
     }
 }
--- Rectangle/test/RectangleTest.java
+++ Rectangle/test/RectangleTest.java
@@ -1,4 +1,3 @@
-import junit.framework.Assert;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -12,18 +11,18 @@ public class RectangleTest {
     private Rectangle rectangle;
 
     @Test
-    public void shouldCalculateAreaWhenLengthIs5andWidthIs4() throws Exception {
+    public void shouldCalculateArea() throws Exception {
         rectangle = new Rectangle(5, 4);
         assertEquals(20, rectangle.area());
     }
 
     @Test
-    public void shouldCalculateAreaWhenLengthAndWidthAreDifferent() throws Exception {
+    public void shouldCalculateAreaGivenAnyLengthAndWidth() throws Exception {
         rectangle = new Rectangle(7, 4);
-        assertEquals(28,rectangle.area());
+        assertEquals(28, rectangle.area());
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void shouldNotAllowRectangleWithNegativeLength() throws Exception {
         rectangle = new Rectangle(-2, 1);
     }
@@ -42,11 +41,30 @@ public class RectangleTest {
     @Test
     public void shouldNotAllowRectangleWithWidthOfZero() throws Exception {
         try {
-            new Rectangle(2,0);
+            new Rectangle(2, 0);
             fail("Should have thrown an exception");
 
         } catch (RuntimeException exception) {
-            assertEquals("Length or width cannot be negative", exception.getMessage());
+            assertEquals("Length and width must be positive", exception.getMessage());
         }
+    }
+
+    @Test
+    public void shouldCalculatePerimeter() throws Exception {
+        rectangle = new Rectangle(3, 4);
+        assertEquals(14, rectangle.perimeter());
+    }
+
+    @Test
+    public void shouldCalculatePerimeterGivenAnyLengthAndWidth() throws Exception {
+        rectangle = new Rectangle(2, 3);
+        assertEquals(10, rectangle.perimeter());
+    }
+
+    @Test
+    public void shouldKnowThatASquareIsARectangle() throws Exception {
+        Rectangle rectangle = new Rectangle(2, 2);
+        Rectangle square = Rectangle.asSquare(2);
+        assertThat(square, is(rectangle));
     }
 }
